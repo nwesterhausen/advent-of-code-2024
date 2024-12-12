d4265b301b063ba6ba23e0fb812c8423a683fe4c
Nick Westerhausen <nwesterhausen@gmail.com>
2024-12-12 16:21:53 -0500

    WIP: problem dampener is a real problem

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -7,6 +7,59 @@ pub fn main() {
     let num_safe_reports = reports.iter().filter(|r| is_safe_report(r)).count();
 
     println!("Part 1 - Number of safe reports: {}", num_safe_reports);
+
+    let unsafe_reports = reports.iter().filter(|r| !is_safe_report(r)).collect::<Vec<&Vec<i32>>>();
+    let num_safe_reports_with_problem_dampener = unsafe_reports.iter().filter(|r| is_safe_report_with_problem_dampener(r)).count();
+    
+    println!("Part 2 - Number of safe reports with problem dampener: {}", num_safe_reports_with_problem_dampener);
+    println!("Part 2 - Total number of 'safe' reports: {}", num_safe_reports + num_safe_reports_with_problem_dampener);
+}
+
+/// Check if the report passes with a problem dampener and tolerate a single invalid level
+fn is_safe_report_with_problem_dampener(report: &Vec<i32>) -> bool {
+    let (is_valid, problem_dampener) = is_homogenous_differences_with_problem_dampener(report);
+    let mut problem_dampener = problem_dampener;
+    if !is_valid {
+        return false;
+    }
+
+    for i in 1..report.len() {
+        // if our problem dampener is already used, we can't use it again. but we can use it once
+        if !is_valid_difference(report[i - 1], report[i]) {
+            if problem_dampener {
+                return false;
+            }
+            problem_dampener = true;
+        }
+    }
+
+    true
+}
+
+/// We can tolerate a single invalid level in a report.
+/// 
+/// Returns a tuple of two booleans:
+/// - If the report is valid with a problem dampener
+/// - If we had to use the problem dampener
+fn is_homogenous_differences_with_problem_dampener(report: &Vec<i32>) -> (bool, bool) {
+    let mut increasing = true;
+    let mut decreasing = true;
+    let mut problem_dampener = false;
+
+    for i in 1..report.len() {
+        if report[i] > report[i - 1] {
+            decreasing = false;
+        } else if report[i] < report[i - 1] {
+            increasing = false;
+        } else {
+            if problem_dampener {
+                return (false, true);
+            }
+            problem_dampener = true;
+        }
+    }
+
+    (increasing || decreasing, problem_dampener)
 }
 
 /// Return true if the report is valid
